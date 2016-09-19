d22e7115f82db1765bb58be0ddf99006e8765cdb
aishwaryathavasi <aishu30.ak@gmail.com>
2016-09-19 09:22:44 +0530

    message

---
+++ Project/EducationProject/src/com/niit/mks/MyDriver.java
@@ -0,0 +1,30 @@
+package com.niit.mks;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MyDriver {
+	public static void main(String [] args) throws IOException, InterruptedException, ClassNotFoundException
+	{
+		Configuration conf=new Configuration();
+		Job job=new Job(conf, "education census");
+		job.setJarByClass(MyDriver.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job,new Path (args[0]));
+        FileOutputFormat.setOutputPath(job,new Path (args[1]));
+        System.exit(job.waitForCompletion(true)?0:1);
+        
+        
+	}
+	
+
+}
